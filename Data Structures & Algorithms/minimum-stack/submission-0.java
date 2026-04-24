dd57725495f1ee0a0fb61ad9e564b04a506b66ec
Oluwasemilore Omotade-Michaels <semiloreomotade@gmail.com>
2026-04-25 00:54:16 +0100

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,27 @@
+class MinStack {
+
+    Deque<Integer> stack;
+    Deque<Integer> minStack;
+    public MinStack() {
+        stack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) minStack.push(val);
+    }
+    
+    public void pop() {
+        int popped = stack.pop();
+        if (popped == minStack.peek()) minStack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
